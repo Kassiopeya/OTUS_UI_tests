6e300092fffbffdbc0cf3e0ec9589b2988196a90
kassiopeya <plesovskih.olga@yandex.ru>
2024-12-08 20:17:14 +0300

    Добавлен docker-compose файл

--- tests/test_example.py
+++ tests/test_example.py
@@ -6,14 +6,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-browser = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-ssl-errors=yes')
+options.add_argument('--ignore-certificate-errors')
+browser = webdriver.Chrome(options)
+opencart_url = "https://127.0.0.1/"
+
 
 def test_header_appeared():
-    browser.get("https://demo.opencart.com/")
+    browser.get(opencart_url)
     assert "Your Store" in browser.title, "Название 'Your Store' не найдено"
 
 def test_item_card_clickable():
-    browser.get("https://demo.opencart.com/")
+    browser.get(opencart_url)
     WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"content\"]/div[2]/div[1]/div/div[1]/a")))
     item_card_clickable = browser.find_element(By.XPATH, "//*[@id=\"content\"]/div[2]/div[1]/div/div[1]/a")
     item_card_clickable.click()
@@ -21,7 +26,7 @@ def test_item_card_clickable():
     assert "MacBook" in browser.title, "Название 'MacBook' не найдено"
 
 def test_add_to_cart():
-    browser.get("https://demo.opencart.com/")
+    browser.get(opencart_url)
     WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, "//*[@id=\"content\"]/div[2]/div[1]/div/div[2]/form/div/button[1]")))
     add_to_cart_button = browser.find_element(By.XPATH, "//*[@id=\"content\"]/div[2]/div[1]/div/div[2]/form/div/button[1]")
     add_to_cart_button.click()
@@ -32,7 +37,7 @@ def test_add_to_cart():
 
 @pytest.mark.parametrize( "number", [1, 2, 3, 8])
 def test_dropdown_shown(number):
-    browser.get("https://demo.opencart.com/")
+    browser.get(opencart_url)
     navbar_button = browser.find_element(By.XPATH, f"//*[@id=\"narbar-menu\"]/ul/li[{number}]/a/ya-tr-span")
     dropdown_menu = browser.find_element(By.XPATH, f"//*[@id=\"narbar-menu\"]/ul/li[{number}]/div")
     navbar_button.click()
