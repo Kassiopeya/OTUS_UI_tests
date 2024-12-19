49c62faefed20c0e1256d9570d8a829a2986c3f0
kassiopeya <plesovskih.olga@yandex.ru>
2024-12-19 21:19:50 +0300

    Добавлены тесты

---
+++ tests/Pages.py
@@ -0,0 +1,133 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+def wait_until_displayed_by_id(browser, locator):
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.ID, f"{locator}")))
+
+def wait_until_displayed_by_xpath(browser, locator):
+    WebDriverWait(browser, 2).until(EC.visibility_of_element_located((By.XPATH, f"{locator}")))
+
+class MainPage:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def click_mac_book_item_card(self, browser):
+        my_locator = "//*[@id=\"content\"]/div[2]/div[1]/div/div[1]/a"
+        wait_until_displayed_by_xpath(browser, my_locator)
+        item_card_clickable = browser.find_element(By.XPATH, my_locator)
+        item_card_clickable.click()
+
+    def check_sum_changed(self, browser):
+        sum_text = browser.find_element(By.XPATH, "//*[@id=\"header-cart\"]/div/button")
+        return sum_text.text
+
+# тут бага - тыкаешь кнопку checout - попадаешь на страницу Shopping Cart
+    def click_checkout_button(self, browser):
+        checkout_button = browser.find_element(By.CSS_SELECTOR, "#top>div>div.nav.float-end>ul>li:nth-child(5)>a>span")
+        checkout_button.click()
+
+    def check_content_text(self, browser):
+        browser.find_element(By.XPATH, "//*[@id=\"content\"]/h1[contains(text(), 'Checkout')]")
+
+    def click_wishlist_button(self, browser):
+        my_locator = "//*[@id=\"wishlist-total\"]/span"
+        checkout_button = browser.find_element(By.XPATH, my_locator)
+        wait_until_displayed_by_xpath(browser, my_locator)
+        checkout_button.click()
+
+    def click_new_customer_continue(self, browser):
+        my_locator = "//*[@id=\"content\"]/div/div[1]/div/div/a"
+        wait_until_displayed_by_xpath(browser, my_locator)
+        continue_button = browser.find_element(By.XPATH, my_locator)
+        continue_button.click()
+
+class ItemCard:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def click_add_to_cart(self, browser):
+        my_locator = "button-cart"
+        wait_until_displayed_by_id(browser, my_locator)
+        add_to_cart_button = browser.find_element(By.ID, my_locator)
+        add_to_cart_button.click()
+
+class LoginPage:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def account_login_block_is_displayed(self, browser):
+        my_locator = "account-login"
+        wait_until_displayed_by_id(browser, my_locator)
+        account_login_block = browser.find_element(By.ID, my_locator)
+        return account_login_block.is_displayed()
+
+class RegistrationPage:
+    def __init__(self, browser):
+        self.browser = browser
+
+    def account_register_block_is_displayed(self, browser):
+        my_locator = "account-register"
+        wait_until_displayed_by_id(browser, my_locator)
+        account_register_block = browser.find_element(By.ID, my_locator)
+        return account_register_block.is_displayed()
+
+    def fill_in_name(self, browser, name):
+        my_locator = "input-firstname"
+        input_first_name = browser.find_element(By.ID, my_locator)
+        input_first_name.send_keys(name)
+
+    def fill_in_last_name(self, browser, lastname):
+        my_locator = "input-lastname"
+        input_first_name = browser.find_element(By.ID, my_locator)
+        input_first_name.send_keys(lastname)
+
+    def fill_in_email(self, browser, email):
+        my_locator = "input-email"
+        input_first_name = browser.find_element(By.ID, my_locator)
+        input_first_name.send_keys(email)
+
+    def fill_in_password(self, browser, password):
+        my_locator = "input-password"
+        input_first_name = browser.find_element(By.ID, my_locator)
+        input_first_name.send_keys(password)
+
+    def click_switch_privacy_policy(self, browser):
+        my_locator = "//*[@id=\"form-register\"]/div/div/input"
+        switch_privacy_policy = browser.find_element(By.XPATH, my_locator)
+        switch_privacy_policy.click()
+
+    def click_submit(self, browser):
+        submit_button = browser.find_element(By.XPATH, "//*[@id=\"form-register\"]/div/button")
+        submit_button.click()
+
+    def success_registration_text_is_displayed(self, browser):
+        my_locator = "common-success"
+        wait_until_displayed_by_id(browser, my_locator)
+        success_registration_text = browser.find_element(By.ID, my_locator)
+        return success_registration_text.is_displayed()
+
+    def check_email_error(self, browser):
+        my_locator = "error-email"
+        wait_until_displayed_by_id(browser, my_locator)
+        error_text = browser.find_element(By.ID, my_locator)
+        return error_text.is_displayed()
+
+    def check_firstname_error(self, browser):
+        my_locator = "error-firstname"
+        wait_until_displayed_by_id(browser, my_locator)
+        error_text = browser.find_element(By.ID, my_locator)
+        return error_text.is_displayed()
+
+    def check_lastname_error(self, browser):
+        my_locator = "error-lastname"
+        wait_until_displayed_by_id(browser, my_locator)
+        error_text = browser.find_element(By.ID, my_locator)
+        return error_text.is_displayed()
+
+    def check_password_error(self, browser):
+        my_locator = "error-password"
+        wait_until_displayed_by_id(browser, my_locator)
+        error_text = browser.find_element(By.ID, my_locator)
+        return error_text.is_displayed()
--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,16 +1,21 @@
+import random
 import time
-import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from tests.MainPage import MainPage
+from tests.Pages import MainPage, ItemCard, LoginPage, RegistrationPage
 
 options = webdriver.ChromeOptions()
 options.add_argument('--ignore-ssl-errors=yes')
 options.add_argument('--ignore-certificate-errors')
 browser1_ = webdriver.Chrome(options)
 opencart_url = "https://127.0.0.1/"
+time_sleep_value = 1
 
 main_page = MainPage(browser1_)
+item_card = ItemCard(browser1_)
+login_page = LoginPage(browser1_)
+registration_page = RegistrationPage(browser1_)
+random_number = random.randint(100, 900)
 
 def test_header_appeared():
     browser1_.get(opencart_url)
@@ -18,24 +23,90 @@ def test_header_appeared():
 
 def test_item_card_clickable():
     browser1_.get(opencart_url)
-    main_page.wait_mac_book_item_card(browser1_)
     main_page.click_mac_book_item_card(browser1_)
-    time.sleep(2)
+    time.sleep(time_sleep_value)
     assert "MacBook" in browser1_.title, "Название 'MacBook' не найдено"
 
 def test_add_to_cart():
     browser1_.get(opencart_url)
-    main_page.wait_mac_book_item_card(browser1_)
     main_page.click_mac_book_item_card(browser1_)
-    time.sleep(2)
-    main_page.click_add_to_cart(browser1_)
-    time.sleep(2)
+    item_card.click_add_to_cart(browser1_)
+    time.sleep(time_sleep_value)
     assert main_page.check_sum_changed(browser1_)=="1 item(s) - $602.00"
 
-@pytest.mark.parametrize( "number", [1, 2, 3, 8])
-def test_dropdown_shown(number):
+def test_checkout_button_unauth():
+    # тест падает - в опенкарте баг - жмешь чекаут - попадаешь в корзину - пример для проверки скриншота
     browser1_.get(opencart_url)
-    navbar_button = browser1_.find_element(By.XPATH, f"//*[@id=\"narbar-menu\"]/ul/li[{number}]/a/ya-tr-span")
-    dropdown_menu = browser1_.find_element(By.XPATH, f"//*[@id=\"narbar-menu\"]/ul/li[{number}]/div")
-    navbar_button.click()
-    assert dropdown_menu.is_displayed()
+    time.sleep(time_sleep_value)
+    main_page.click_checkout_button(browser1_)
+    time.sleep(time_sleep_value)
+    checkout_register_element = browser1_.find_element(By.ID, "checkout-register")
+    assert checkout_register_element.is_displayed()
+
+def test_wishlist_button_unauth():
+    browser1_.get(opencart_url)
+    main_page.click_wishlist_button(browser1_)
+    assert login_page.account_login_block_is_displayed(browser1_) == True
+
+def test_register_form_displayed():
+    browser1_.get(opencart_url)
+    main_page.click_wishlist_button(browser1_)
+    main_page.click_new_customer_continue(browser1_)
+    assert  registration_page.account_register_block_is_displayed(browser1_)==True
+
+def test_registration_happy_path():
+    browser1_.get(opencart_url)
+    main_page.click_wishlist_button(browser1_)
+    main_page.click_new_customer_continue(browser1_)
+    registration_page.fill_in_name(browser1_, f"Victoria{random_number}")
+    registration_page.fill_in_last_name(browser1_, f"Ivanova{random_number}")
+    registration_page.fill_in_email(browser1_, f"test@test{random_number}.ru")
+    registration_page.fill_in_password(browser1_, f"123{random_number}")
+    registration_page.click_switch_privacy_policy(browser1_)
+    registration_page.click_submit(browser1_)
+    assert registration_page.success_registration_text_is_displayed(browser1_) == True
+
+def test_negative_registration_empty_email():
+    browser1_.get(opencart_url)
+    main_page.click_wishlist_button(browser1_)
+    main_page.click_new_customer_continue(browser1_)
+    registration_page.fill_in_name(browser1_, f"Victoria{random_number}")
+    registration_page.fill_in_last_name(browser1_, f"Ivanova{random_number}")
+    registration_page.fill_in_password(browser1_, f"123{random_number}")
+    registration_page.click_switch_privacy_policy(browser1_)
+    registration_page.click_submit(browser1_)
+    assert registration_page.check_email_error(browser1_) == True
+
+def test_negative_registration_empty_name():
+    browser1_.get(opencart_url)
+    main_page.click_wishlist_button(browser1_)
+    main_page.click_new_customer_continue(browser1_)
+    registration_page.fill_in_last_name(browser1_, f"Ivanova{random_number}")
+    registration_page.fill_in_email(browser1_, f"test@test{random_number}.ru")
+    registration_page.fill_in_password(browser1_, f"123{random_number}")
+    registration_page.click_switch_privacy_policy(browser1_)
+    registration_page.click_submit(browser1_)
+    assert registration_page.check_firstname_error(browser1_) == True
+
+
+def test_negative_registration_empty_last_name():
+    browser1_.get(opencart_url)
+    main_page.click_wishlist_button(browser1_)
+    main_page.click_new_customer_continue(browser1_)
+    registration_page.fill_in_name(browser1_, f"Victoria{random_number}")
+    registration_page.fill_in_email(browser1_, f"test@test{random_number}.ru")
+    registration_page.fill_in_password(browser1_, f"123{random_number}")
+    registration_page.click_switch_privacy_policy(browser1_)
+    registration_page.click_submit(browser1_)
+    assert registration_page.check_lastname_error(browser1_) == True
+
+def test_negative_registration_empty_password():
+    browser1_.get(opencart_url)
+    main_page.click_wishlist_button(browser1_)
+    main_page.click_new_customer_continue(browser1_)
+    registration_page.fill_in_name(browser1_, f"Victoria{random_number}")
+    registration_page.fill_in_last_name(browser1_, f"Ivanova{random_number}")
+    registration_page.fill_in_email(browser1_, f"test@test{random_number}.ru")
+    registration_page.click_switch_privacy_policy(browser1_)
+    registration_page.click_submit(browser1_)
+    assert registration_page.check_password_error(browser1_) == True
